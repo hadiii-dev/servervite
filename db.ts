7d8d2065f0155e7863b38afd712cb1785fa5c515
Ahmad Hassan <h.ah.ahmad00@gmail.com>
2025-04-04 23:49:27 +0500

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,7 +1,5 @@
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-
-neonConfig.fetchConnectionCache = true;
 
 // Check if DATABASE_URL is defined in environment variables
 const databaseUrl = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_xzTpcRQeu53r@ep-rapid-grass-a59kwmg5-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require';
